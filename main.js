1a3d43e3f64e5eb3534e616a34637e2286a6edd6
Magmusacy <xpfk.098@gmail.com>
2020-03-07 18:04:57 +0100

    Created the logic which assigns pre operator value to x and post operator value to y

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-let add = (x,y) => x+y;
-let substract = (x,y) => x-y; 
-let multiply = (x,y) => x*y;
-let divide = (x,y) => x/y;
-
+// Even though only addition requires number I replaced all the strings with numbers
+let add = (x,y) => Number(x)+Number(y);
+let substract = (x,y) => Number(x)-Number(y); 
+let multiply = (x,y) => Number(x)*Number(y);
+let divide = (x,y) => Number(x)/Number(y);
+const calcDisplay = document.querySelector('#display')
+calcDisplay.textContent = '0';
+const numbers = document.querySelectorAll('.num')
+const operators = document.querySelectorAll('.op')
+const clearBtn = document.querySelector('.clr')
+const decimal = document.querySelector('.dot')
+let x = ""
+let y = ""
 function operate(operator,x,y) {
     switch(operator) {
         case add:
@@ -15,6 +23,64 @@ function operate(operator,x,y) {
             return divide(x,y)
         }  
 }
+    const thing = (obj) => {
+        const op = obj.target.textContent
+        switch(op) {
+            case 'x':
+                action = multiply;
+                x = calcDisplay.textContent;
+                calcDisplay.textContent = "0";
+                return console.log('x')
+        }
+        switch(op) {
+            case '/':
+                action = divide;
+                x = calcDisplay.textContent;
+                calcDisplay.textContent = "0";
+                return console.log('/')
+        }
+        switch(op) {
+            case '+':
+                action = add;
+                x = calcDisplay.textContent;
+                calcDisplay.textContent = "0";
+                return console.log('+')
+        }
+        switch(op) {
+            case '-':
+                action = substract;
+                x = calcDisplay.textContent;
+                calcDisplay.textContent = "0";
+                return console.log('-')
+        }
+        switch(op) {
+            case '=':
+                y = calcDisplay.textContent;
+                calcDisplay.textContent = operate(action,x,y)
+                return console.log('=')
+        }
+    }
 
-const chuj = document.querySelector('button')       
-console.log(chuj.value)
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click', (e) => {
+            if (calcDisplay.textContent === '0') {
+                calcDisplay.textContent = ""
+            }
+            calcDisplay.textContent += e.target.textContent
+        })
+    }
+
+    for (let i = 0; i < operators.length; i++) {
+       operators[i].addEventListener('click', thing)
+}
+
+clearBtn.addEventListener('click', () => {
+    calcDisplay.textContent = '0';
+    x = 0;
+}) 
+decimal.addEventListener('click', () => {
+    if (calcDisplay.textContent.includes('.')) {
+        return;
+    } calcDisplay.textContent += '.'
+})
+
